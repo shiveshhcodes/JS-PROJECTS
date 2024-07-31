3bdd294ea7fe512838ab060b714e831b4686f9f4
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-01 02:24:44 +0530

    Update app.js

--- STONE PAPER SCISSOR/app.js
+++ STONE PAPER SCISSOR/app.js
@@ -2,10 +2,57 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice")
+const msgg = document.querySelector("#msgg");
+const genCompChoice = () =>{
+    const options = ["rock" , "paper" , "scissors"];
+    const randIdx = Math.floor(Math.random()*3);
+    return options[randIdx];
+    // rock-paper-scissor
+}
+
+const drawGame = () =>{
+    console.log("game was draw.");
+    msgg.innerText = "Game was Draw!! Play Again"
+}
+
+const showWinner = (userWin) => {
+    if (userWin) {
+        console.log("You Win!!");
+        msgg.innerText = "You Won!!"
+    } else {
+        console.log("You Lose");
+    }
+};
+
+const playGame = (userChoice) =>{
+    console.log("user Choice = " , userChoice);
+    const compChoice = genCompChoice();
+    console.log("computer choice = ", compChoice)
+
+    if (userChoice===compChoice){
+        drawGame();
+    } else {
+        let userWin = true;
+        if (userChoice === "rock") {
+            // paper - scissor
+            userWin = compChoice === "paper" ? false : true;
+        }
+        else if(userChoice === "paper") {
+            // rock , scissor
+            userWin = compChoice === "scissiors" ? false : true;
+        }
+        else{
+            compChoice === "rock" ? false : true ;
+        }
+        showWinner(userWin);
+    }
+};
 choices.forEach((choice) =>{
     console.log(choice)
     choice.addEventListener("click" , () => {
-        const choiceID = choice.getAttribute("id");
-    console.log(choiceID , "choice was clicked")
+        const userChoice = choice.getAttribute("id");
+    // console.log(userChoice, "choice was clicked")l
+    playGame(userChoice)
     })
 }) 
+
